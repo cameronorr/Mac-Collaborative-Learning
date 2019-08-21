af1a7f0a7def892b52dd5281213a2bca2dc12581
Cameron Orr <50644803+cameronorr@users.noreply.github.com>
2019-08-21 13:09:18 -0400

    Prepared for deploy.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -19,6 +20,17 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/questions', require('./routes/questions'));
 app.use('/api/comments', require('./routes/comments'));
 app.use('/api/votes', require('./routes/votes'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // takes all the url pathways entered by a user and redirects them to the index.html file in the build folder
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 // Sets port to 5000 if there is no other port set.
 const PORT = process.env.PORT || 5000;
